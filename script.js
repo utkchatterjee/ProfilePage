9a26a9b2b7df7d3bd2a0f4e124cb5c17305a0f72
utkchatterjee <utkchatterjee@gmail.com>
2026-06-08 16:04:20 +0530

    Stylistic changes to quote section

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ function dark()
         btn.style.color='black';
     }
 }
+
+const qot=document.getElementById('Quote')
+async function FetchQuote()
+{
+    //random number to insert in fetch
+    let random=Math.floor(Math.random()*100);
+    try{
+        let response=await fetch(`https://dummyjson.com/quotes/${random}`);
+        let data=await response.json();
+        let quotation=data.quote;
+        let auth=data.author;
+        qot.textContent=`${quotation}      -${auth}`;
+    }
+    catch(error)
+    {
+        console.error(error);
+    }
+}
